1355593957edc934862bef90aa1f757fc2355909
ArenaDuels <ashtont2026+capsule@gmail.com>
2026-07-16 10:13:36 -0600

    Add new mansion and mega-towers maps to arena list

--- js/arenas.js
+++ js/arenas.js
@@ -1,9 +1,11 @@
 import classic from "./arenas/classic.js";
 import pillars from "./arenas/pillars.js";
 import towers from "./arenas/towers.js";
+import mansion from "./arenas/mansion.js";
+import megaTowers from "./arenas/mega-towers.js";
 
-export const ARENAS = { classic, pillars, towers };
-export const ARENA_LIST = [classic, pillars, towers];
+export const ARENAS = { classic, pillars, towers, mansion, "mega-towers": megaTowers };
+export const ARENA_LIST = [classic, pillars, towers, mansion, megaTowers];
 
 export function getArena(id) {
   return ARENAS[id] || ARENAS.classic;
